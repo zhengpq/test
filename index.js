84b2dc1705c6c81fa9cd550e51a990248f198ce5
zhengpq <1455760759@qq.com>
2020-11-03 12:18:18 +0000

    Update from https://github.com/zhengpq/adui-icons/commit/c369b37d7ab1f6cfbf95361ea7f92773dc67abc5

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import dataURIData from './lib/data/data-URI/data-URI-data'
-import typeData from './lib/data/icon-types/type-data'
-import svgData from './lib/data/svg/svg-data'
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 
-export default {
-  svgData,
-  typeData,
-  dataURIData,
-}
+var _dataURIData = _interopRequireDefault(require("./lib/data/data-URI/data-URI-data"));
+
+var _typeData = _interopRequireDefault(require("./lib/data/icon-types/type-data"));
+
+var _svgData = _interopRequireDefault(require("./lib/data/svg/svg-data"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = {
+  svgData: _svgData["default"],
+  typeData: _typeData["default"],
+  dataURIData: _dataURIData["default"]
+};
+exports["default"] = _default;
--- node_modules/figma-js/build/module/figmaTypes.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZmlnbWFUeXBlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9maWdtYVR5cGVzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
